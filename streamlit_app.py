755ec60f9a8f33d4a38f81e2d4e8dbcfb2b0ce1c
yvh242 <58807680+yvh242@users.noreply.github.com>
2025-04-23 12:38:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Hello Streamlit-er 👋")
+st.markdown(
+    """ 
+    This is a playground for you to try Streamlit and have fun. 
+
+    **There's :rainbow[so much] you can build!**
+    
+    We prepared a few examples for you to get started. Just 
+    click on the buttons above and discover what you can do 
+    with Streamlit. 
+    """
 )
+
+if st.button("Send balloons!"):
+    st.balloons()
